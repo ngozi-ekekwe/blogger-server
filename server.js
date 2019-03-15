4771eac4c8e726f6c8031422c830fd52139a4ce8
ngozi-ekekwe <ngozi.ekekwe@andela.com>
2019-03-15 19:43:14 +0100

    set up express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+port = process.env.PORT || 9000;
+
+app.get("*", (req, res) => {
+    res.status(201).send({
+        message: "My todo Api"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`)
+});
+
+module.exports = app;
