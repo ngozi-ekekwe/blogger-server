fe84c9e2fed7a6933dfaa295702cab9faaeba70e
ngozi-ekekwe <ngozi.ekekwe@andela.com>
2019-03-18 07:00:39 +0100

    version API

--- server/routes.js
+++ server/routes.js
@@ -4,15 +4,15 @@ const blogController = require('./controller').blogController;
 const verifyToken = require('./middleware/auth').verifyToken
 
 module.exports = (app) => {
-  app.post('/api/user', userController.create);
-  app.post('/api/login', userController.login);
-  app.get('/api/users', userController.list);
-  app.get('/api/users/:userId', userController.retrieve)
+  app.post('/api/v1/user', userController.create);
+  app.post('/api/v1/login', userController.login);
+  app.get('/api/v1/users', userController.list);
+  app.get('/api/v1/users/:userId', userController.retrieve)
 
-  app.get('/api/stories', blogController.list);
-  app.post('/api/new-story', verifyToken, blogController.create);
-  app.put('/api/story/:userId/:slug', verifyToken,  blogController.update);
-  app.delete('/api/story/:userId/:slug', verifyToken, blogController.destroy);
-  app.get('/api/story/:slug', blogController.retrieve)
-  app.get('/api/stories/:userId', verifyToken, blogController.listMyBlogs)
+  app.get('/api/v1/stories', blogController.list);
+  app.post('/api/v1/new-story', verifyToken, blogController.create);
+  app.put('/api/v1/story/:userId/:slug', verifyToken,  blogController.update);
+  app.delete('/api/v1/story/:userId/:slug', verifyToken, blogController.destroy);
+  app.get('/api/v1/story/:slug', blogController.retrieve)
+  app.get('/api/v1/stories/:userId', verifyToken, blogController.listMyBlogs)
 }
